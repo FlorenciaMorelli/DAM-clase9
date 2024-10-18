997914fa0c27e9db3db971ce6427970f578e48e4
Florencia Morelli <florenciamorelliIT@gmail.com>
2024-10-18 20:20:59 -0300

    APK created

--- app/src/main/java/com/example/clase9/MainActivity.kt
+++ app/src/main/java/com/example/clase9/MainActivity.kt
@@ -5,7 +5,11 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.clase9.databinding.ActivityMainBinding
+import com.example.clase9.model.Student
+import com.example.clase9.model.StudentRepository
+import com.example.clase9.view.StudentAdapter
 
 class MainActivity : AppCompatActivity() {
 
@@ -17,5 +21,12 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
 
+        initRecycler(StudentRepository.students)
+
+    }
+
+    private fun initRecycler(students: List<Student>) {
+        binding.rvStudents.layoutManager = LinearLayoutManager(this)
+        binding.rvStudents.adapter = StudentAdapter(students)
     }
 }
---
+++ app/src/main/java/com/example/clase9/model/StudentRepository.kt
@@ -0,0 +1,35 @@
+package com.example.clase9.model
+
+class StudentRepository {
+    companion object{
+        val students = listOf<Student>(
+            Student(1, "Florencia", "Morelli", "12345678", "Buenos Aires", "1234"),
+            Student(2, "Juan", "Gómez", "31234567", "Córdoba", "5000"),
+            Student(3, "Sofía", "Rodríguez", "32345678", "Rosario", "2000"),
+            Student(4, "Mateo", "López", "33456789", "Mendoza", "5500"),
+            Student(5, "Valentina", "Martínez", "34567890", "La Plata", "1900"),
+            Student(6, "Tomás", "Fernández", "35678901", "Mar del Plata", "7600"),
+            Student(7, "Martina", "González", "36789012", "Salta", "4400"),
+            Student(8, "Santiago", "Torres", "37890123", "Santa Fe", "3000"),
+            Student(9, "Catalina", "Vera", "38901234", "Tucumán", "4000"),
+            Student(10, "Joaquín", "Castro", "39012345", "Neuquén", "8300"),
+            Student(11, "Lola", "Ramírez", "40123456", "San Juan", "5400"),
+            Student(12, "Emiliano", "Sosa", "41234567", "Río Gallegos", "9400"),
+            Student(13, "Florencia", "Silva", "42345678", "Corrientes", "3400"),
+            Student(14, "Gabriel", "Moreno", "43456789", "San Luis", "5700"),
+            Student(15, "Victoria", "Herrera", "44567890", "Jujuy", "4600"),
+            Student(16, "Benjamín", "Rivas", "45678901", "Posadas", "3300"),
+            Student(17, "Camila", "Medina", "46789012", "San Fernando", "1646"),
+            Student(18, "Ignacio", "Figueroa", "47890123", "Trelew", "9100"),
+            Student(19, "Isabella", "Cruz", "48901234", "Viedma", "8500"),
+            Student(20, "Sebastián", "Ortega", "49012345", "Resistencia", "3500"),
+            Student(21, "Milagros", "Rossi", "50123456", "Catamarca", "4700"),
+            Student(22, "Agustín", "Díaz", "51234567", "Villa María", "5900"),
+            Student(23, "Julieta", "Ponce", "52345678", "Bahía Blanca", "8000"),
+            Student(24, "Lucas", "Iglesias", "53456789", "Formosa", "3600"),
+            Student(25, "Mía", "Bravo", "54567890", "Goya", "3450")
+        )
+    }
+
+
+}
