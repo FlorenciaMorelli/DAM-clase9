25248b35e263332660aa72fd46dbb7a985c3c91d
Florencia Morelli <florenciamorelliIT@gmail.com>
2024-10-18 19:41:44 -0300

    Main Activity layout and Student data class

---
+++ app/src/main/java/com/example/clase9/model/Student.kt
@@ -0,0 +1,10 @@
+package com.example.clase9.model
+
+data class Student(
+    val id: Int,
+    val name: String,
+    val lastName: String,
+    val dni: String,
+    val city: String,
+    val cp: String
+)
