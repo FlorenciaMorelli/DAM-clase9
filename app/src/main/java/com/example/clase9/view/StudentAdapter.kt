8cf80887ac506b07c585065dc5d65ceb9d3a1e33
Florencia Morelli <florenciamorelliIT@gmail.com>
2024-10-18 20:02:15 -0300

    Fully implemented MVVM

--- app/src/main/java/com/example/clase9/view/StudentAdapter.kt
+++ app/src/main/java/com/example/clase9/view/StudentAdapter.kt
@@ -1,4 +1,21 @@
 package com.example.clase9.view
 
-class StudentAdapter {
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.clase9.R
+import com.example.clase9.model.Student
+
+class StudentAdapter (val students: List<Student>) : RecyclerView.Adapter<StudentViewHolder>() {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): StudentViewHolder {
+        val layoutInflater = LayoutInflater.from(parent.context).inflate(R.layout.student_item, parent, false)
+        return StudentViewHolder(layoutInflater)
+    }
+
+    override fun getItemCount() = students.size
+
+    override fun onBindViewHolder(holder: StudentViewHolder, position: Int) {
+        val item = students[position]
+        holder.bind(item)
+    }
 }
--- app/src/main/java/com/example/clase9/view/StudentViewHolder.kt
+++ app/src/main/java/com/example/clase9/view/StudentViewHolder.kt
@@ -1,4 +1,21 @@
 package com.example.clase9.view
 
-class StudentViewHolder {
+import android.view.View
+import androidx.recyclerview.widget.RecyclerView
+import com.example.clase9.databinding.ActivityMainBinding
+import com.example.clase9.databinding.StudentItemBinding
+import com.example.clase9.model.Student
+
+class StudentViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+
+    val binding = StudentItemBinding.bind(view)
+
+    fun bind(student: Student){
+        binding.tvName.text = student.name
+        binding.tvLastname.text = student.lastName
+        binding.tvDni.text = student.dni
+        binding.tvCity.text = student.city
+        binding.tvCp.text = student.cp
+    }
+
 }
